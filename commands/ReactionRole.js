ce0536a69fd04dc7c3410b91603cedc2fb5f4c84
Nubo <72999487+Nuboctane@users.noreply.github.com>
2021-11-17 09:45:41 +0100

    Add files via upload

---
+++ commands/ReactionRole.js
@@ -0,0 +1,49 @@
+
+module.exports = {
+    name: 'reactionrole',
+    description: "Sets up a reaction role message!",
+    async execute(message, args, Discord, client) {
+        const channel = '910120387436769300';
+        const role = message.guild.roles.cache.find(role => role.name === "members");
+        const emoji = '✅';
+  
+        let msg = "just click the god damn checkbox";
+  
+        let messageEmbed = await message.channel.send(msg);
+        messageEmbed.react(emoji);
+  
+        client.on('messageReactionAdd', async (reaction, user) => {
+            if (reaction.message.partial) await reaction.message.fetch();
+            if (reaction.partial) await reaction.fetch();
+            if (user.bot) return;
+            if (!reaction.message.guild) return;
+  
+            if (reaction.message.channel.id == channel) {
+                if (reaction.emoji.name === emoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.add(role);
+                }
+            } else {
+                return;
+            }
+  
+        });
+  
+        client.on('messageReactionRemove', async (reaction, user) => {
+  
+            if (reaction.message.partial) await reaction.message.fetch();
+            if (reaction.partial) await reaction.fetch();
+            if (user.bot) return;
+            if (!reaction.message.guild) return;
+  
+  
+            if (reaction.message.channel.id == channel) {
+                if (reaction.emoji.name === role) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(role);
+                }
+            } else {
+                return;
+            }
+        });
+    }
+  
+  }   
--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const Discord = require('discord.js');
 
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"], partials: ["CHANNEL"] });
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES", "GUILD_MESSAGE_REACTIONS"], partials: ["CHANNEL"] });
 
-const PREFIX = '.';
 
 client.once('ready', () => {
   console.log("glubo bot online, dm bot to open bots dm's");
@@ -13,17 +12,26 @@ client.once('ready', () => {
   });
 });
 
-
-
-
 let chance = 0;
-let chance2 = 0;
+let PERFIX = '-';
 let responding = false;
 let THEY = null;
+
+ 
+const fs = require('fs');
+ 
+client.commands = new Discord.Collection();
+ 
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+ 
+    client.commands.set(command.name, command);
+}
+
 client.on('message', message => {
   //
   if (message.channel.type == "DM"){ //if its a dm
-
   }
   if (message.mentions.has(client.user)) {
     if (chance == 0) {
@@ -44,5 +52,17 @@ client.on('message', message => {
     }
   }
 });
+  client.on('message', message => {
+ 
+    if (!message.content.startsWith(PERFIX) || message.author.bot) return;
+ 
+    const args = message.content.slice(PERFIX.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+    if (command === 'reactionrole') {
+        client.commands.get('reactionrole').execute(message, args, Discord, client);
+    } 
+  
+});
 
 
+client.login("OTEwMDgwNjE0NDEyMjE0MzAz.YZNong.jTP0FVUFAuidoe1uOUTWmXpQenc");
